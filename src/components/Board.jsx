3fff2bf538ac5923f2372c802a03ff0954a462a8
laion220995 <laion220995@gmail.com>
2018-12-16 18:07:42 +0700

    transfer logic from child to parent component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,47 +2,9 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      xIsNext: true,
-    };
-  }
-
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i += 1) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a]
-        && squares[a] === squares[b]
-        && squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   handleClick(i) {
-    const { xIsNext } = this.state;
-    const { squares, changeData } = this.props;
-
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? 'X' : 'O';
-    this.setState({ xIsNext: !xIsNext });
-    changeData(squares);
+    const { handleClick } = this.props;
+    handleClick(i);
   }
 
   renderSquare(i) {
@@ -51,20 +13,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const { xIsNext } = this.state;
-    const { squares } = this.props;
-
-    const winner = this.calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
     return (
       <div className="board">
-        <div className="board-status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,24 +6,78 @@ class Game extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      status: 'Next turn: X',
+      xIsNext: true,
     };
   }
 
-  changeData(squares) {
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i += 1) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a]
+        && squares[a] === squares[b]
+        && squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  changeStatus(xIsNext) {
+    const { squares } = this.state;
+
+    const winner = this.calculateWinner(squares);
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
+    this.setState({ status });
+  }
+
+  handleClick(i) {
+    const { squares, xIsNext } = this.state;
+
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
     this.setState({ squares });
+    this.setState({ xIsNext: !xIsNext });
+    this.changeStatus(!xIsNext);
   }
 
   render() {
-    const { squares } = this.state;
+    const { squares, xIsNext, status } = this.state;
 
     return (
       <div className="game">
         <div className="game-board">
           <Board
             squares={squares}
-            changeData={() => this.changeData(squares)}
+            xIsNext={xIsNext}
+            changeSquares={() => this.changeSquares(squares)}
+            changeXIsNext={() => this.changeXIsNext()}
+            handleClick={i => this.handleClick(i)}
           />
         </div>
+        <h6>{status}</h6>
       </div>
     );
   }
